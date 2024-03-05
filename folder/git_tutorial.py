1ca11cbe36b40d4e07a19e9e845475ff2ee9f08a
Morgan-Sell <morganpsell@gmail.com>
2024-03-05 18:17:39 -0500

    added docstring to add_numbers()

--- folder/git_tutorial.py
+++ folder/git_tutorial.py
@@ -1,5 +1,10 @@
 def  add_numbers(a, b, c):
+    """Return the sum of a, b, and c."""
     return a + b + c
 
+# 1. extend add_numbers to allow an arbitrary number of parameters
+# 2. investigate and resolve security explort of add_numbers()
+# 3. make the implementaiton of add_numbers() more performant
+# 4. add documentation for add_numbers()
 def calc_difference(a, b):
     return a - b
